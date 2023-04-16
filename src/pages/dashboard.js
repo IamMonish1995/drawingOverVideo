c430d1d437d6266e1535cb929a9c24c5017bb839
monish <monishbarse9@gmail.com>
2023-04-16 12:26:06 +0530

    card added

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,19 +1,8 @@
-"use client";
-import React, { useState, useEffect } from "react";
-
-import CssBaseline from "@mui/material/CssBaseline";
-import { DashboardLayout } from "@/components/dashboard-layout";
-
 export default function Dashboard() {
   return (
     <>
-      <CssBaseline/>
       Dashboard
     </>
   );
 }
-Dashboard.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
-);
+
